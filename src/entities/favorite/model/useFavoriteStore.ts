f73bd25e183ce89ac27402bcda4a25cdfef7269e
jiyuunyang <jiyuunyang@gmail.com>
2026-02-15 12:29:16 +0900

    fix: useFavoriteStore에 UI 코드 삭제, FavoriteButton에 경고 UI 팝업 로직 추가

--- src/entities/favorite/model/useFavoriteStore.ts
+++ src/entities/favorite/model/useFavoriteStore.ts
@@ -6,7 +6,7 @@ import { normalize } from '@/shared/lib/utils/normalize';
 
 interface FavoriteState {
   favorites: Favorite[];
-  addFavorite: (name: string, nickname?: string) => void;
+  addFavorite: (name: string) => { success: boolean; message: string };
   editFavorite: (name: string, nickname: string) => void;
   removeFavorite: (name: string) => void;
 }
@@ -18,13 +18,17 @@ export const useFavoriteStore = create<FavoriteState>()(
 
       addFavorite: (name) => {
         if (get().favorites.length >= 6) {
-          alert('즐겨찾기는 최대 6개까지 추가할 수 있습니다.');
-          return;
+          return {
+            success: false,
+            message: '최대 6개까지 추가할 수 있습니다.',
+          };
         }
         const locationInfo = koreaLocationsXY.find(
           (d) => normalize(d.name.replaceAll('-', ' ')) === normalize(name),
         );
-        if (!locationInfo) return;
+        if (!locationInfo) {
+          return { success: false, message: '해당 위치를 추가할 수 없습니다' };
+        }
 
         const { lat, lon, x, y } = locationInfo;
 
@@ -38,6 +42,7 @@ export const useFavoriteStore = create<FavoriteState>()(
         };
 
         set({ favorites: [...get().favorites, newFavorite] });
+        return { success: true, message: '즐겨찾기 추가' };
       },
 
       // nickname 수정
--- src/entities/favorite/ui/FavoriteButton.tsx
+++ src/entities/favorite/ui/FavoriteButton.tsx
@@ -1,5 +1,6 @@
 import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
 import { useFavoriteStore } from '../model/useFavoriteStore';
+import { useState } from 'react';
 
 interface FavoriteButtonProps {
   location: string;
@@ -8,33 +9,63 @@ interface FavoriteButtonProps {
 export function FavoriteButton({ location }: FavoriteButtonProps) {
   const { favorites, addFavorite, removeFavorite } = useFavoriteStore();
   const isFavorite = favorites.some((f) => f.name === location);
+  const [message, setMessage] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   const toggleFavorite = () => {
     if (isFavorite) {
       removeFavorite(location);
     } else {
-      addFavorite(location);
+      const result = addFavorite(location);
+      if (result.success === false) {
+        setMessage(result.message);
+        openModal();
+      }
     }
   };
 
   return (
-    <button
-      onClick={toggleFavorite}
-      className='relative inline-block group text-2xl cursor-pointer'
-    >
-      {isFavorite ? (
-        <MdFavorite className='text-pink-500' />
-      ) : (
-        <MdFavoriteBorder className='text-gray-400' />
-      )}
-      {/* 툴팁 */}
-      <div
-        className='absolute bottom-full left-1/2 transform -translate-x-1/2 
+    <>
+      <button
+        onClick={toggleFavorite}
+        className='relative inline-block group text-2xl cursor-pointer'
+      >
+        {isFavorite ? (
+          <MdFavorite className='text-pink-500' />
+        ) : (
+          <MdFavoriteBorder className='text-gray-400' />
+        )}
+        {/* 툴팁 */}
+        <div
+          className='absolute bottom-full left-1/2 transform -translate-x-1/2 
                     w-max max-w-xs bg-black text-white text-sm rounded-md px-1 py-0.5 -mt-1
                     opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none z-10'
-      >
-        즐겨찾기
-      </div>
-    </button>
+        >
+          즐겨찾기
+        </div>
+      </button>
+      {/* 모달 */}
+      {isOpen && (
+        <div className='fixed inset-0 bg-black/50 z-50 flex items-center justify-center p-8'>
+          <div className='bg-card-background dark:bg-card-background-dark rounded-2xl p-5 w-80'>
+            <h3 className='text-md mb-3'>{message}</h3>
+            <div className='mt-4 flex justify-end gap-2'>
+              <button
+                onClick={closeModal}
+                className='w-full px-3 py-1 rounded-md bg-green-500 text-white cursor-pointer'
+              >
+                확인
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
